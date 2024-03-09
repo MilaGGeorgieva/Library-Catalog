1124ac8b599e3cbeeb1cfae4474a98ea8988b0bd
MilaGGeorgieva <milaggeorgieva@gmail.com>
2024-03-09 13:29:21 +0200

    Added tests for Login and Register buttons

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -5,5 +5,21 @@ test("Verify All Books link is visible", async ( {page} ) => {
     await page.waitForSelector("nav.navbar");
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isLinkVisible = await allBooksLink.isVisible();
-    expect(isLinkVisible).toBe(true);
-})
+    expect(isLinkVisible).toBe(true); 
+});
+
+test("Verify Login buttnon is visible", async ( {page} ) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("nav.navbar");
+    const loginButton = await page.$('a[href="/login"]');
+    const isLoginButtonVisisble = await loginButton.isVisible();
+    expect(isLoginButtonVisisble).toBe(true); 
+});
+
+test("Verify Register buttnon is visible", async ( {page} ) => {
+    await page.goto("http://localhost:3000");
+    await page.waitForSelector("nav.navbar");
+    const registerButton = await page.$('a[href="/register"]');
+    const isRegisterButtonVisisble = await registerButton.isVisible();
+    expect(isRegisterButtonVisisble).toBe(true); 
+});
