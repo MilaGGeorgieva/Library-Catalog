920eba4998e61aa41420979a7f00fecfe7db4049
MilaGGeorgieva <milaggeorgieva@gmail.com>
2024-03-10 19:17:39 +0200

    add test for logge-in users

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,7 +1,9 @@
 const { expect, test} = require('@playwright/test');
+const baseURL = "http://localhost:3000";
+const loggedInURL = "http://localhost:3000/login"
 
 test("Verify All Books link is visible", async ( {page} ) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(baseURL);
     await page.waitForSelector("nav.navbar");
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isLinkVisible = await allBooksLink.isVisible();
@@ -9,7 +11,7 @@ test("Verify All Books link is visible", async ( {page} ) => {
 });
 
 test("Verify Login buttnon is visible", async ( {page} ) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(baseURL);
     await page.waitForSelector("nav.navbar");
     const loginButton = await page.$('a[href="/login"]');
     const isLoginButtonVisisble = await loginButton.isVisible();
@@ -17,9 +19,29 @@ test("Verify Login buttnon is visible", async ( {page} ) => {
 });
 
 test("Verify Register buttnon is visible", async ( {page} ) => {
-    await page.goto("http://localhost:3000");
+    await page.goto(baseURL);
     await page.waitForSelector("nav.navbar");
     const registerButton = await page.$('a[href="/register"]');
     const isRegisterButtonVisisble = await registerButton.isVisible();
     expect(isRegisterButtonVisisble).toBe(true); 
+});
+
+test.only("Verify All Books link is visible after login", async ( {page} ) => {
+    //await page.goto(baseURL);
+    //await page.waitForSelector("nav.navbar");
+    //await page.click('a[href="/login"]');
+    
+    await page.goto(loggedInURL);
+    //await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('#email', "peter@abv.bg");
+    await page.fill('#password', "123456");
+    await page.click('#login-form > fieldset > input');
+    
+    const logoutButton = await page.$('#logoutBtn');
+    const isLogoutButtonVisible = await logoutButton.isVisible();
+    expect(isLogoutButtonVisible).toBe(true); 
+
+    //const allBooksLink = await page.$('a[href="/catalog"]');
+    //const isLinkVisible = await allBooksLink.isVisible();
+    //expect(isLinkVisible).toBe(true); 
 });
