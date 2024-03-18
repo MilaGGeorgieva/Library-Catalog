156a823d80e5476099e5a1d4f4f15d3daba5e888
MilaGGeorgieva <milaggeorgieva@gmail.com>
2024-03-18 20:18:28 +0200

    Added 3 more tests after login

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -1,6 +1,6 @@
 const { expect, test} = require('@playwright/test');
 const baseURL = "http://localhost:3000";
-const loggedInURL = "http://localhost:3000/login"
+const loginURL = "http://localhost:3000/login"
 
 test("Verify All Books link is visible", async ( {page} ) => {
     await page.goto(baseURL);
@@ -31,7 +31,7 @@ test("Verify All Books link is visible after login", async ( {page} ) => {
     //await page.waitForSelector("nav.navbar");
     //await page.click('a[href="/login"]');
     
-    await page.goto(loggedInURL);
+    await page.goto(loginURL);
     //await page.fill('input[name="email"]', 'peter@abv.bg');
     await page.fill('#email', "peter@abv.bg");
     await page.fill('#password', "123456");
@@ -44,4 +44,37 @@ test("Verify All Books link is visible after login", async ( {page} ) => {
     //const allBooksLink = await page.$('a[href="/catalog"]');
     //const isLinkVisible = await allBooksLink.isVisible();
     //expect(isLinkVisible).toBe(true); 
+});
+
+test("Verify My Books link is visible after login", async ( {page} ) => {
+    await page.goto(loginURL);    
+    await page.fill('#email', "peter@abv.bg");
+    await page.fill('#password', "123456");
+    await page.click('#login-form > fieldset > input');    
+    
+    const myBooksButton = await page.$('a[href="/profile"]');
+    const isMyBooksButtonVisible = await myBooksButton.isVisible();
+    expect(isMyBooksButtonVisible).toBe(true);     
+});
+
+test("Verify Add Book link is visible after login", async ( {page} ) => {
+    await page.goto(loginURL);    
+    await page.fill('#email', "peter@abv.bg");
+    await page.fill('#password', "123456");
+    await page.click('#login-form > fieldset > input');    
+    
+    const addBookButton = await page.$('a[href="/create"]');
+    const isAddBookButtonVisible = await addBookButton.isVisible();
+    expect(isAddBookButtonVisible).toBe(true);     
+});
+
+test("Verify User's email address is visible after login", async ( {page} ) => {
+    await page.goto(loginURL);    
+    await page.fill('#email', "peter@abv.bg");
+    await page.fill('#password', "123456");
+    await page.click('#login-form > fieldset > input');    
+    
+    const emailInfo = await page.$('#user > span');
+    const isEmailInfoVisible = await emailInfo.isVisible();
+    expect(isEmailInfoVisible).toBe(true);     
 });
